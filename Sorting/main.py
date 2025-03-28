7cda3ab0f9e66364113f6acc3526d269e4253259
CodeIsMySpotter <codeismyspotter@gmail.com>
2025-03-28 10:45:55 +0100

    Algorithms -- Sorting -- Adding base test classes

---
+++ Sorting/main.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+class Generator():
+
+  def __init__(self):
+    self.lengths = [10, 10**2, 10**3, 10**4, 10**5, 10**6]
+
+  def generate_random(self):
+    pass
+
+  def generate_A_random(self):
+    pass
+
+  def generate_V_random(self):
+    pass
+
+
+class Test:
+
+  def __init__(self, tests: list, generator: Generator):
+    self.tests = tests
+    self.generator = generator
+
+
+
+class Queue():
+
+  def __init__(self, test: Test, ):
+    self.test = test
+
+
+  def run_test():
+    pass
+
+
+  def export_plot():
+    pass
+
+
+def main():
+  pass
+
+if __name__ == "__main__":
+  main()
+
+
+
