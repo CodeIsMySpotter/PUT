6ffd67e9e8805e9378a86cdd443a2952f38ffa64
CodeIsMySpotter <codeismyspotter@gmail.com>
2025-04-02 22:26:59 +0200

    Algorithms -- Tree -- Created array generator and main function

--- Tree/main.py
+++ Tree/main.py
@@ -1,2 +1,17 @@
 import sqlite3
 
+from generator import *
+from BST import *
+from Node import *
+
+
+
+def main():
+  BSTTree = BST()
+  array = Generator.generate_int(100, 10)
+
+  BSTTree.insert([5, 4, 6, 3, 7, 2, 8, 1, 9])
+  BSTTree.pre_order()
+
+
+main()
