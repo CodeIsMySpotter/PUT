3a43bfa2e345d9e1820c00266786885fb95bbced
CodeIsMySpotter <codeismyspotter@gmail.com>
2025-05-01 23:30:43 +0200

    ENA -- Arithmetics -- Made functions generics

--- ENA/Arithmetics/interpolation.cpp
+++ ENA/Arithmetics/interpolation.cpp
@@ -8,18 +8,19 @@
 using namespace boost::multiprecision;
 using namespace std;
 
-tuple<float128, int> lagrange_interpolation(const vector<float128>& x, const vector<float128>& y, float128 x_val) {
+template<typename T>
+tuple<T, int> lagrange_interpolation(const vector<T>& x, const vector<T>& y, T x_val) {
     int st = check_conditions(x);
     if (st == 1) {
-        return make_tuple(0.0, 1);
+        return make_tuple(T(0), 1);
     } else if (st == 2) {
-        return make_tuple(0.0, 2);
+        return make_tuple(T(0), 2);
     }
 
-    float128 result = 0;
+    T result = 0;
     size_t n = x.size();
     for (size_t i = 0; i < n; ++i) {
-        float128 term = y[i];
+        T term = y[i];
         for (size_t j = 0; j < n; ++j) {
             if (i != j) {
                 term *= (x_val - x[j]) / (x[i] - x[j]);
@@ -31,16 +32,17 @@ tuple<float128, int> lagrange_interpolation(const vector<float128>& x, const vec
 }
 
 
-tuple<float128, int> neville_interpolation(const vector<float128>& x, const vector<float128>& y, float128 x_val) {
+template<typename T>
+tuple<T, int> neville_interpolation(const vector<T>& x, const vector<T>& y, T x_val) {
     int st = check_conditions(x);
     if (st == 1) {
-        return make_tuple(0.0, 1);
+        return make_tuple(T(0), 1);
     } else if (st == 2) {
-        return make_tuple(0.0, 2);
+        return make_tuple(T(0), 2);
     }
 
     size_t n = x.size();
-    vector<float128> p = y;
+    vector<T> p = y;
 
     for (size_t i = 1; i < n; ++i) {
         for (size_t j = n - 1; j >= i; --j) {
@@ -50,8 +52,10 @@ tuple<float128, int> neville_interpolation(const vector<float128>& x, const vect
     return make_tuple(p[n - 1], 0);
 }
 
-int check_conditions(const vector<float128>& x) {
-    set<float128> unique_elements(x.begin(), x.end());
+
+template<typename T>
+int check_conditions(const vector<T>& x) {
+    set<T> unique_elements(x.begin(), x.end());
     if (unique_elements.size() != x.size()) {
         return 2;
     }
